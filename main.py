f699ead80b70e8b77e01a86b503fc432bc7b5469
Ulrich Lindberg <ulrich.olsen@gmail.com>
2025-10-15 11:58:21 +0200

    Updates

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 import subprocess
 import tempfile
 import shutil
+import torch
+from nnunetv2.paths import nnUNet_results
+from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
+from nnunetv2.utilities.dataset_name_id_conversion import convert_id_to_dataset_name
 
 def main(suv_file_path,seg_out_file_path):
     results_dir = os.environ.get("nnUNet_results",None)
@@ -17,12 +21,45 @@ def main(suv_file_path,seg_out_file_path):
         
         #subprocess.check_output(["curl", "https://zenodo.org/records/13691360/files/Dataset214_LUMIERE_default_TL.zip?download=1", "|", "bsdtar", "-xvf-", "-C", results_dir],shell=True)
         subprocess.check_output(["unzip","/homes/ulrich/projects/AortaPETSeg/nnUNet_results/weights.zip", "-d", results_dir])
+
+    # TESTING
+    # Configuration        
+    dataset = 300
+    configuration = '3d_fullres'
+    trainer = 'nnUNetTrainer'
+    plans = 'nnUNetResEncUNetMPlans'
+    folds = (0,1,2,3,4)
         
+    # instantiate the nnUNetPredictor
+    predictor = nnUNetPredictor(
+        tile_step_size=0.5,
+        use_gaussian=True,
+        use_mirroring=True,
+        perform_everything_on_device=True,
+        device=torch.device('cuda'),
+        verbose=False,
+        verbose_preprocessing=False,
+        allow_tqdm=True
+    )
+    
+    # initializes the network architecture, loads the checkpoint
+    predictor.initialize_from_trained_model_folder(
+        os.path.join(nnUNet_results, convert_id_to_dataset_name(dataset), f'{trainer}__{plans}__{configuration}'),
+        use_folds=folds
+    )
     
     with tempfile.TemporaryDirectory() as in_dir:
         with tempfile.TemporaryDirectory() as out_dir:
-            shutil.copy(suv_file_path,in_dir+"/in_0000.nii.gz")
-            subprocess.check_output(["nnUNetv2_predict", "-i", str(in_dir), "-o", str(out_dir), "-d", "300", "-f", "0", "1", "2", "3", "4", "-p", "nnUNetResEncUNetMPlans", "-c", "3d_fullres"])
+            shutil.copy(suv_file_path,os.path.join(in_dir, "in_0000.nii.gz"))
+            # Does inference
+            predictor.predict_from_files(
+                [os.path.join(in_dir, "in_0000.nii.gz")], [os.path.join(out_dir, "segmentation.nii.gz")],
+                save_probabilities=False, overwrite=False,
+                num_processes_preprocessing=2, num_processes_segmentation_export=2,
+                folder_with_segs_from_prev_stage=None, num_parts=1, part_id=0
+            )
+
+            # subprocess.check_output(["nnUNetv2_predict", "-i", str(in_dir), "-o", str(out_dir), "-d", "300", "-f", "0", "1", "2", "3", "4", "-p", "nnUNetResEncUNetMPlans", "-c", "3d_fullres"])
         
             out_file = [os.path.join(out_dir, x) for x in os.listdir(out_dir) if str(x).endswith(".nii.gz")]
             assert len(out_file) == 1
